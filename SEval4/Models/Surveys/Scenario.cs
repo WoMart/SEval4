999d8d7f3009784cb2ec50a48c61eb038050db55
Marcin Woznicki <m.woznicki.16@abdn.ac.uk>
2021-04-25 20:23:58 +0100

    Add models for questionnaire question and answer

---
+++ SEval4/Models/Surveys/Scenario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEval4.Models
+{
+    public class Scenario
+    {
+        [Key]
+        public Guid ScenarioId { get; set; }
+
+        public string Context { get; set; }
+
+        public Guid Correct { get; set; }
+
+        public virtual ICollection<ScenarioResponse> Responses { get; set; }
+    }
+}
---
+++ SEval4/Models/Surveys/ScenarioResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEval4.Models
+{
+    public class ScenarioResponse : BaseTextValuePair<int>
+    {
+        public string Tag { get; set; }
+
+        public bool Correct { get; set; }
+
+        public Guid ScenarioId { get; set; }
+
+        public virtual Scenario Scenario { get; set; }
+    }
+}
