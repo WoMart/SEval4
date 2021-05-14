cbd3fe87ed4afdaae2662338b2db8e629e28cc7c
Marcin Woznicki <m.woznicki.16@abdn.ac.uk>
2021-05-14 17:37:25 +0100

    Implement fetch for scenarios for BaselineSurvey

---
+++ SEval4/Models/BaselineFormAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEval4.Models
+{
+    public class BaselineFormAnswer
+    {
+        public int ScenarioId { get; set; }
+        public string Value { get; set; }
+    }
+}
