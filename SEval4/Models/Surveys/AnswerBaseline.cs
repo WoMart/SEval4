09972303a3cfbf858bc9ea1ffbcdff2b908e5c04
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-16 19:33:59 +0100

    Change BaselineFormAnswer name to AnswersBaseline

--- SEval4/Models/Surveys/AnswerBaseline.cs
+++ SEval4/Models/Surveys/AnswerBaseline.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SEval4.Models
 {
-    public class BaselineFormAnswer
+    public class AnswerBaseline
     {
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
