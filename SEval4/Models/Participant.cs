313df9acb2c914453c3fa14102bb462fee41bcd7
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-16 20:24:54 +0100

    Add Participant model

---
+++ SEval4/Models/Participant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEval4.Models
+{
+    public class Participant
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public DateTime CreationTime { get; set; }
+        public int AllocatedGroup { get; set; }
+        public DateTime AllocationTime { get; set; }
+        [Required]
+        public bool IsFinished { get; set; }
+    }
+}
