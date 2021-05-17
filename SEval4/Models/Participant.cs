4c1d7a0da130d9610df494bd61a2cbb88df5c605
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-17 17:06:31 +0100

    Add model for Participant

--- SEval4/Models/Participant.cs
+++ SEval4/Models/Participant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,15 @@ namespace SEval4.Models
 {
     public class Participant
     {
+        [Key]
         public Guid Id { get; set; }
         [Required]
-        public DateTime CreationTime { get; set; }
-        public int AllocatedGroup { get; set; }
-        public DateTime AllocationTime { get; set; }
-        [Required]
         public bool IsFinished { get; set; }
+        [Required]
+        public DateTime CreationTime { get; set; }
+        public DateTime? AllocationTime { get; set; }
+        [ForeignKey(nameof(StudyGroup))]
+        public int? StudyGroupId { get; set; }
+        public virtual StudyGroup StudyGroup { get; set; }
     }
 }
