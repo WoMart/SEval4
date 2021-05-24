60d60f27c702d6e320ee11eeb1d66f96f0e01256
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-24 22:00:36 +0100

    Add entity class for single user answer in evaluation

---
+++ SEval4/Models/Surveys/AnswerEvaluation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SEval4.Models
+{
+    public class AnswerEvaluation
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Participant))]
+        public Guid UserId { get; set; }
+        public int ScenarioId { get; set; }
+        public int ResponseOrder { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public virtual Participant Participant { get; set; }
+    }
+}
