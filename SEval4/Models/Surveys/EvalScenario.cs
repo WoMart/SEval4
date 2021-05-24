519a0cf413cbd89c0fb6cc08383d0777e08957d5
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-24 16:16:45 +0100

    Add Scenario and response classes for evaluation

---
+++ SEval4/Models/Surveys/EvalScenario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEval4.Models
+{
+    public class EvalScenario : Scenario
+    {
+        public string Feedback { get; set; }
+        [NotMapped]
+        public bool IsAnsweredCorrectly { get; set; } = false;
+
+    }
+}
