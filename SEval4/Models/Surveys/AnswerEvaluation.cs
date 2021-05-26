9b007eab8b0289e5903c772cea836f9097aa6592
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-26 18:27:23 +0100

    Add DateTime to EvaluationAnswer

--- SEval4/Models/Surveys/AnswerEvaluation.cs
+++ SEval4/Models/Surveys/AnswerEvaluation.cs
@@ -10,8 +10,9 @@ namespace SEval4.Models
         public int Id { get; set; }
         [ForeignKey(nameof(Participant))]
         public Guid UserId { get; set; }
+        public DateTime InsertTime { get; set; }
         public int ScenarioId { get; set; }
-        public int ResponseOrder { get; set; }
+        public int ResponseValue { get; set; }
         public bool IsCorrect { get; set; }
 
         public virtual Participant Participant { get; set; }
