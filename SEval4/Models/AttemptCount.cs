14cb12f3f091f573ea7f464017c1eb43fd016969
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-27 18:11:59 +0100

    Add model to record the attempt count

---
+++ SEval4/Models/AttemptCount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEval4.Models
+{
+    public class AttemptCount
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Participant))]
+        public Guid UserId { get; set; }
+        public int ScenarioId { get; set; }
+        public int Attempts { get; set; }
+
+        public virtual Participant Participant { get; set; }
+    }
+}
