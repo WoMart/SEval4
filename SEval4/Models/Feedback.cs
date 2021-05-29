f3fcc117580fd91a958f14f552f78378bb779178
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-05-29 20:23:40 +0100

    Add model for Feedback Survey responses

---
+++ SEval4/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEval4.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+
+    }
+}
