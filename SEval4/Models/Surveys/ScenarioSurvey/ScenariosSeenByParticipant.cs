ff3644bc61e71ce3135ac80beb312e58d2295f92
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-06-02 18:16:17 +0100

    Add a model to record scenarios seen by participant

---
+++ SEval4/Models/Surveys/ScenarioSurvey/ScenariosSeenByParticipant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEval4.Models
+{
+    public class ScenariosSeenByParticipant
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string InSurvey { get; set; }
+
+        [Required]
+        public int ScenarioId { get; set; }
+    }
+}
