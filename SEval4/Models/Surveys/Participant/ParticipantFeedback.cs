b2f37f996418bb925a3342f48b64ff868c391bd8
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-06-04 17:14:29 +0100

    Add new model for participant feedback and remove the old unused one

---
+++ SEval4/Models/Surveys/Participant/ParticipantFeedback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEval4.Models
+{
+    public class ParticipantFeedback
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public DateTime TimeStamp { get; set; }
+
+        // Feedback form
+        [Required]
+        public string StudyHelpfulness { get; set; }
+        [Required]
+        public string PhishingConfidenceChange { get; set; }
+        [Required]
+        public string SpearPhishingConfidenceChange { get; set; }
+        
+        // Text answers, optional
+        public string ScenarioFeedback { get; set; }
+        public string TrainingFeedback { get; set; }
+        public string GeneralFeedback { get; set; }
+    }
+}
