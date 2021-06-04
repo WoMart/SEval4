0bd40fd093d48260a398bd98be54029d7d5b5a77
Marcin Woźnicki <m.woznicki.16@aberdeen.ac.uk>
2021-06-04 12:31:22 +0100

    Add SurveyName to SurveyAnswer

--- SEval4/Models/Surveys/ScenarioSurvey/SurveyAnswer.cs
+++ SEval4/Models/Surveys/ScenarioSurvey/SurveyAnswer.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SEval4.Models
 {
     public class SurveyAnswer : BaseAnswer
     {
+        [Required]
+        public string SurveyName { get; set; }
+
         //[ForeignKey(nameof(ResponseValue))]
         //public virtual Response Response { get; set; }
     }
